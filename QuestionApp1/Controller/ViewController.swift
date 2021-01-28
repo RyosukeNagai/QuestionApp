761740e8d739f6e76859cb66855991ac2db200e9
vagrant <vagrant@localhost.localdomain>
2021-01-28 07:25:03 +0000

    Buttonを繋ぐ

--- QuestionApp1/Controller/ViewController.swift
+++ QuestionApp1/Controller/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var maxScoreLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func answer(_ sender: Any) {
+        
+        if (sender as AnyObject).tag == 1{
+            
+            //丸ボタンが押された時
+            
+            //ユーザーが押したボタンが丸ボタンだった
+            
+            // 丸ボタンの音声を流す
+            
+        }else if (sender as AnyObject).tag == 2{
+            
+            //×ボタンが押された時
+            
+            //ユーザーが押したボタンが罰ボタンだった
+            
+            // 罰ボタンの音声を流す
+        }
+        
+    }
+    
+    
+    
+    
 }
 
